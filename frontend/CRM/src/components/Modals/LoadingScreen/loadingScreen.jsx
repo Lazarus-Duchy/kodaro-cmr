537065967c24b7c2642c45cd688e948546c39d96
EszkernyMati <mateuszpoppe-kaca@elektronikonline.pl>
2026-02-18 21:28:59 +0100

    Added the login and register buttons with its forms

--- frontend/CRM/src/components/Modals/LoadingScreen/loadingScreen.jsx
+++ frontend/CRM/src/components/Modals/LoadingScreen/loadingScreen.jsx
@@ -1,7 +1,15 @@
 import React from "react";
 import "./loadingScreen.css";
 
-const loadingScreen = ()=>{
+const loadingScreen = () => {
+  return (
+    <div className="loading-container">
+      <div className="loader-wrapper">
+        <div className="client-flow-loader"></div>
+        <div className="loader-text">ClientFlow</div>
+      </div>
+    </div>
+  );
+};
 
-}
 export default loadingScreen;
